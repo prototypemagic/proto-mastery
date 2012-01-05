920e8e312c0a744a51fbba905d1343de2a048b05
AJ Bahnken <avb.wkyhu@gmail.com>
2012-01-05 15:15:47 -0800

    adding wrong coins

--- codechef/aj/COINS_ajs_wrong.py
+++ codechef/aj/COINS_ajs_wrong.py
@@ -1,15 +1,15 @@
 
 def max_if_exchanged(num):
     return num/2 + num/3 + num/4
-
+     
 def check(num):
     exchanged = max_if_exchanged(num)
     return num if num > exchanged else exchanged
-
+              
 if __name__ == "__main__":
     while True:
-        try:
-            n = int( raw_input().strip() )
-            print check(n)
-        except:
-            break
+       try:
+           n = int(raw_input())
+           print check(n)
+       except:
+           break
