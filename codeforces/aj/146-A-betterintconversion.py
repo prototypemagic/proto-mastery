156282ccfbfeef324073bbbbdbe9e8f3570ea09a
AJ Bahnken <avb.wkyhu@gmail.com>
2012-01-25 19:20:51 -0800

    Down to three lines

--- codeforces/aj/146-A-betterintconversion.py
+++ codeforces/aj/146-A-betterintconversion.py
@@ -1,5 +1,5 @@
 
 half = int(raw_input()) / 2 #What is inputted is the amount of characters in the 'ticket'. 
 t = map(int, raw_input())  #I am just popping it straight into what will be half of the ticket size
-print "YES" if t.count(4) + t.count(7) == len(t) and sum(t[half:]) == sum(t[:half]) else print "NO"
+print "YES" if t.count(4) + t.count(7) == len(t) and sum(t[half:]) == sum(t[:half]) else "NO"
 
