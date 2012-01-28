51bd3985cfe566e7ab22338ad6c24b6d94aee8ef
Steve Phillips <elimisteve@gmail.com>
2012-01-27 22:56:11 -0800

    Gave AJ credit for his own code. Clarified my tweak of his original

--- codeforces/aj/146-A-modifiedbysteve.py
+++ codeforces/aj/146-A-modifiedbysteve.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python
+# AJ Bahnken / avb_wkyhu
+# 2012.01.21
+
 half = int(raw_input()) / 2
-t    = [int(x) for x in raw_input()]
+t    = ticket_digits = [int(x) for x in raw_input()]
 
 if t.count(4) + t.count(7) == len(t) and sum(t[half:]) == sum(t[:half]):
     print "YES"
